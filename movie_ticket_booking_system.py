f65ec96a0eb52fb2e1f34983f14f4c9021af2521
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-03-31 02:02:54 -0700

    Fix formatting and improve comments in booking system

--- movie_ticket_booking_system.py
+++ movie_ticket_booking_system.py
@@ -1,4 +1,4 @@
-#Python Based A movie ticket booking system:
+#Python Based A Movie ticket booking system:
 def calculate_final_price(base_price, age, seat_type, show_time, is_member, is_weekend):
     
     # Eligibility Check:
